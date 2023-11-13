e4cc9a1dfdba9359dd0ecb1faa4d3db33130e8cf
SgtBagus <procw57@gmail.com>
2023-11-13 12:03:44 +0700

    intial package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
@@ -11,9 +10,9 @@ root.render(
   <AuthContextProvider>
     <ChatContextProvider>
       <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter basename={"/"}>
           <App />
-        </HashRouter>
+        </BrowserRouter>
       </React.StrictMode>
     </ChatContextProvider>
   </AuthContextProvider>
